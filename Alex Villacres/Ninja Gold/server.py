21b0c01c6e8b2f553a48ee7e3cb2d48096bf3288
Alex Villacres <alex71197@yahoo.com>
2016-10-05 18:17:40 -0700

    revised Ninja Gold

--- Alex Villacres/Ninja Gold/server.py
+++ Alex Villacres/Ninja Gold/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, session, request, redirect
-import random, datetime
+from flask import Flask, render_template, redirect, session, request
+import random
+import datetime
 
 app = Flask(__name__)
 app.secret_key = ('thisissecret')
@@ -8,49 +9,45 @@ app.secret_key = ('thisissecret')
 def index():
     if 'gold' not in session:
         session['gold'] = 0
-        print 'Started New Game'
     if 'activity' not in session:
-        session['activity'] = ""
+        session['activity'] = []
     return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
 def process_money():
-    if request.form['action'] == 'farm':
-        gold_added = random.randint(10,20)
-        date = datetime.datetime.now()
+    buildings = {
+        'farm': random.randint(10, 20),
+        'cave': random.randint(5, 10),
+        'house': random.randint(2, 5),
+        'casino': random.randint(-50, 50),
+    }
+    date = datetime.datetime.now()
+    print request.form
+    #checking if the request is working by printing it to terminal
+    print request.form['building']
+    if request.form['building'] in buildings:
+        print 'in if statement'
+        gold_added = buildings[request.form['building']]
         session['gold'] += gold_added
-        session['activity'] += 'Earned {} gold from the farm! ({})'.format(str(gold_added), date)
-        print(session['activity'])
-        print('farmed')
-    if request.form['action'] == 'cave':
-        gold_added = random.randint(5,10)
-        session['gold'] += gold_added
-        date = datetime.datetime.now()
-        session['activity'] += 'Earned {} gold from the cave! ({})'.format(str(gold_added), date)
-        print('caved')
-    if request.form['action'] == 'house':
-        gold_added = random.randint(2,5)
-        session['gold'] += gold_added
-        date = datetime.datetime.now()
-        session['activity'] += 'Earned {} gold from the house! ({})'.format(str(gold_added), date)
-        print('housed')
-    if request.form['action'] == 'casino':
-        plus_or_minus = random.randint(1,2)
-        if plus_or_minus == 1:
-            gold_added = random.randint(0,50)
-            session['gold'] += gold_added
-        else:
-            gold_removed = random.randint(0,50)
-            session['gold'] -= gold_removed
-        date = datetime.datetime.now()
-        print('casino')
-    session['activity'] += "\n"
+        not_casino = "You earned {} gold from the {}!".format(gold_added, request.form['building'])
+        casino = "You entered a casino and {} {} gold".format("won" if gold_added > 0 else "lost", gold_added if gold_added > 0 else -gold_added)
+        print not_casino
+        activity_content = {
+            'activity': not_casino if request.form['building'] != 'casino' else casino,
+            'date': date
+        }
+        session['activity'].append(activity_content)
+        print session['activity']
     return redirect('/')
 
-@app.route('/reset', methods=['POST'])
-def reset():
-    print("Resetting game")
-    del session['gold']
-    del session['activity']
+@app.route('/clear')
+def clear():
+    session.clear()
     return redirect('/')
-app.run(debug=True)
+    
+
+
+
+
+
+app.run(debug = True)
